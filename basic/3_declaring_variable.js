6a81eae588ff2dc49324fef3581119015747da7c
tagrag90 <gunjeepark21@gmail.com>
2023-04-15 12:33:32 +0900

    variable

---
+++ basic/3_declaring_variable.js
@@ -0,0 +1,18 @@
+// 변수 선언하기
+// var - 더 이상 사용하지 않는다. 
+// let
+// const
+
+var name = 'dive to bada';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = "아이브";
+console.log(ive);
+
+// let or var 로 선언하면 값을 추후 변경할 수 있다.
+
+ive = '안유진';
+console.log(ive);
